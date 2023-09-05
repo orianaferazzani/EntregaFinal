a3bf1e71ca5df3377e49b5bfcf4c0f79ac0de8dd
Oriana Ferazzani <oriferazzani1@gmail.com>
2023-09-05 17:55:08 -0300

    finalizado

--- main.js
+++ main.js
@@ -79,3 +79,150 @@ sr.reveal(`.category__data, .trick__content, .footer__content`, {
 sr.reveal(`.about__data, .discount__img`, { origin: "left" });
 sr.reveal(`.about__img, .discount__data`, { origin: "right" });
 
+//Login falso
+$(document).ready(function(){
+    
+  
+  $("#login form").submit(function(){
+      var form_name = $("#form_name").val();
+      localStorage.setItem("form_name", form_name);
+  });
+  var form_name = localStorage.getItem("form_name");
+  if(form_name != null && form_name != "undefined"){
+  var about_parrafo = $("#about p");
+  about_parrafo.html("<br><strong>Bienvenido "+form_name+"!</strong> ");
+  about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>");
+
+  $("#login").hide();
+
+  $("#logout").click(function(){
+    localStorage.clear();
+    location.reload();
+  });
+}
+});
+
+ 
+//pokemon
+const pokemonContainer = document.querySelector(".pokemon-container");
+const spinner = document.querySelector("#spinner"); 
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+let limit = 8;
+let offset = 1;
+
+previous.addEventListener("click", () => {
+  if (offset != 1) {
+    offset -= 9;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+  }
+});
+
+next.addEventListener("click", () => {
+  offset += 9;
+  removeChildNodes(pokemonContainer);
+  fetchPokemons(offset, limit);
+});
+
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      createPokemon(data);
+      spinner.style.display = "none";
+    });
+}
+
+function fetchPokemons(offset, limit) {
+  spinner.style.display = "block";
+  for (let i = offset; i <= offset + limit; i++) {
+    fetchPokemon(i);
+  }
+}
+
+function createPokemon(pokemon) {
+  const flipCard = document.createElement("div");
+  flipCard.classList.add("flip-card");
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card-container");
+
+  flipCard.appendChild(cardContainer);
+
+  const card = document.createElement("div");
+  card.classList.add("pokemon-block");
+
+  const spriteContainer = document.createElement("div");
+  spriteContainer.classList.add("img-container");
+
+  const sprite = document.createElement("img");
+  sprite.src = pokemon.sprites.front_default;
+
+  spriteContainer.appendChild(sprite);
+//numero de pokemon 000 
+  const number = document.createElement("p");
+  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = pokemon.name;
+
+  card.appendChild(spriteContainer);
+  card.appendChild(number);
+  card.appendChild(name);
+
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("pokemon-block-back");
+
+  cardBack.appendChild(progressBars(pokemon.stats));
+
+  cardContainer.appendChild(card);
+  cardContainer.appendChild(cardBack);
+  pokemonContainer.appendChild(flipCard);
+}
+
+function progressBars(stats) {
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
+
+  for (let i = 0; i < 3; i++) {
+    const stat = stats[i];
+
+    const statPercent = stat.base_stat / 2 + "%";
+    const statContainer = document.createElement("stat-container");
+    statContainer.classList.add("stat-container");
+
+    const statName = document.createElement("p");
+    statName.textContent = stat.stat.name;
+
+    const progress = document.createElement("div");
+    progress.classList.add("progress");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar");
+    progressBar.setAttribute("aria-valuenow", stat.base_stat);
+    progressBar.setAttribute("aria-valuemin", 0);
+    progressBar.setAttribute("aria-valuemax", 200);
+    progressBar.style.width = statPercent;
+
+    progressBar.textContent = stat.base_stat;
+
+    progress.appendChild(progressBar);
+    statContainer.appendChild(statName);
+    statContainer.appendChild(progress);
+
+    statsContainer.appendChild(statContainer);
+  }
+
+  return statsContainer;
+}
+
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+fetchPokemons(offset, limit);
